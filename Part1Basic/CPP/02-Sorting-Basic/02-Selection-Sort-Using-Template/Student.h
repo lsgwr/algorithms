435c66651867cd4de1fb011b898c37f2e6adaea0
liang shan guang <1648266192@qq.com>
2018-04-14 11:03:35 +0800

    2-2:选择排序的改进，C++实现地

--- Part1Basic/CPP/02-Sorting-Basic/02-Selection-Sort-Using-Template/Student.h
+++ Part1Basic/CPP/02-Sorting-Basic/02-Selection-Sort-Using-Template/Student.h
@@ -2,4 +2,21 @@
 #define _STUDENT_H_
 #include <iostream>
 using namespace std;
+struct Student{
+
+    string name;
+    int score;
+
+    /* 重载比较运算符 */
+    bool operator<(const Student &otherStudent){
+        // 优先比较分数；分数相等的情况下看名字的字典序；
+        return score != otherStudent.score ? score > otherStudent.score : name < otherStudent.name;
+    }
+    
+    /* 重载输出运算符 */
+    friend ostream& operator<<(ostream &os, const Student &student){
+        os<<"Student: "<< student.name<<" "<<student.score<<endl;
+        return os;
+    }
+};
 #endif // _STUDENT_H_
--- Part1Basic/CPP/02-Sorting-Basic/02-Selection-Sort-Using-Template/main.cpp
+++ Part1Basic/CPP/02-Sorting-Basic/02-Selection-Sort-Using-Template/main.cpp
@@ -6,6 +6,7 @@
  ***********************************************************/
 
 #include <iostream>
+#include "Student.h"
 
 using namespace std;
 /**
@@ -58,5 +59,13 @@ int main() {
         cout << d[i] << " ";
     }
     cout << endl;
+
+    // 5.自定义对象排序
+    Student s[4] = {{"B",78}, {"A", 86},{"G",34}, {"D",78}};
+    selectionSort(s,4);
+    for(int i = 0; i < 4; i++){
+        cout<<s[i];
+    }
+    cout<<endl;
     return 0;
 }
