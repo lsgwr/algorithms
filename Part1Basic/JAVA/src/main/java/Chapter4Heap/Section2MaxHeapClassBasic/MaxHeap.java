2bc0826139d46857a4eda1475a3c188a886c225a
liang shan guang <1648266192@qq.com>
2018-04-17 00:55:38 +0800

    4-3：上浮操作完成

--- Part1Basic/JAVA/src/main/java/Chapter4Heap/Section2MaxHeapClassBasic/MaxHeap.java
+++ Part1Basic/JAVA/src/main/java/Chapter4Heap/Section2MaxHeapClassBasic/MaxHeap.java
@@ -6,20 +6,28 @@
  ***********************************************************/
 package Chapter4Heap.Section2MaxHeapClassBasic;
 
-public class MaxHeap<Item> {
+/**
+ * 在堆的有关操作中，需要比较堆中元素的大小，所以Item需要extends Comparable
+ */
+public class MaxHeap<Item extends Comparable> {
     /**
      * 最大堆中的数据
      */
-    private Item[] data;
+    protected Item[] data;
     /**
      * 最大堆中的元素数量
      */
-    private int count;
+    protected int count;
+    /**
+     * 数组容量
+     */
+    protected int capacity;
 
     public MaxHeap(int capacity) {
         // 不能直接声明泛型数组，只能先声明再强制转换
-        this.data = (Item[]) new Object[capacity];
+        this.data = (Item[]) new Comparable[capacity];
         count = 0;
+        this.capacity = capacity;
     }
 
     /**
@@ -34,10 +42,44 @@ public class MaxHeap<Item> {
     /**
      * 堆是否为空
      */
-    boolean isEmpty() {
+    public boolean isEmpty() {
         return count == 0;
     }
 
+
+    /**
+     * 交换堆中索引为i和j的两个元素
+     */
+    private void swap(int i, int j) {
+        Item t = data[i];
+        data[i] = data[j];
+        data[j] = t;
+    }
+
+    /**
+     * 上浮的私有函数
+     */
+    public void shiftUp(int k) {
+        // k > 1是因为到根节点时就没父节点了，不需要再比较了
+        while (k > 1 && data[k / 2].compareTo(data[k]) < 0) {
+            // 如果当前元素的父元素(在数组中的下表为整除2)比当前元素还小，
+            // 则不符合最大堆的定义，那么就交换一下
+            swap(k / 2, k);
+            k /= 2;
+        }
+    }
+
+    /**
+     * 插入元素
+     */
+    public void insert(Item item) {
+        // 防止后面的++count越界
+        assert (count + 1 <= capacity);
+        // 插入新元素，元素数加1,之所以用++count而不用count++是因为数组下标从1开始
+        data[++count] = item;
+        // 把新加入的元素向上浮动到合适位置
+        shiftUp(count);
+    }
 
     public static void main(String[] args) {
         MaxHeap<Integer> maxHeap = new MaxHeap<>(100);
