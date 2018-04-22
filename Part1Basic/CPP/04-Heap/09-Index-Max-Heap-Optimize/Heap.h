57a1ff07f2d571991f200755aa75010dab7e7baa
liang shan guang <1648266192@qq.com>
2018-04-22 17:45:26 +0800

    4-9 索引堆优化看完了，C++版本

--- Part1Basic/CPP/04-Heap/09-Index-Max-Heap-Optimize/Heap.h
+++ Part1Basic/CPP/04-Heap/09-Index-Max-Heap-Optimize/Heap.h
@@ -12,6 +12,8 @@ private:
     Item* data;
     // 索引数组,用于等效替代data中的元素来进行元素移动(上移和下移)
     int* indexes;
+    // 索引索引数组的数组。reverse[i]表示索引i在indexes（堆）中的位置
+    int* reverses;
     // 元素数量
     int count;
     // 数组容量
@@ -23,6 +25,8 @@ private:
             // 如果当前元素的父元素(在数组中的下表为整除2)比当前元素还小，
             // 则不符合最大堆的定义，那么就交换一下
             swap(indexes[k/2], indexes[k]);
+            reverses[indexes[k/2]] = k/2;
+            reverses[indexes[k]] = k;
             k /= 2;
         }
     }
@@ -44,6 +48,9 @@ private:
             }
             // 父节点小于孩子节点的较大值，那么就和较大值的子节点交换.注意交换是交换indexes
             swap(indexes[k], indexes[j]);
+            // 维护reverses数组
+            reverses[indexes[k]] = k;
+            reverses[indexes[j]] = j;
             // 把换后的子节点作为父节点，接着往下走
             k = j;
         }
@@ -53,11 +60,15 @@ public:
     IndexMaxHeap(int capacity){
         data = new Item[capacity + 1];
         indexes = new int[capacity + 1];
+        reverses = new int[capacity + 1];
+        for(int i = 0; i <= capacity; i++){
+            reverses[i] = 0;
+        }
         count = 0;
         this->capacity = capacity;
     }
 
-    // // 直接输入完整的数组进行最大堆化(Heapify).这块没做好，暂时不用
+    // // 直接输入完整的数组进行最大堆化(Heapify)，这个没放还没处理好，等技术好了回来好好看
     // // 核心在于从(n/2结果取整)下标位置的元素开始到下标为0的元素依次执行shiftDown,好好理解4-5老师前8分钟的图示
     // IndexMaxHeap(Item arr[], int n){
     //     // 声明空间，还没赋值
@@ -65,8 +76,7 @@ public:
     //     capacity = n;
     //     // 挨个赋值
     //     for(int i = 0; i < n; i++){
-    //         data[i+1] = arr[i];
-    //         // 初始化索引，反正在shiftDown的时候也会处理好地
+    //         data[i + 1] = arr[i];
     //         indexes[i+1] = i;
     //     }
     //     // 堆中元素个数为n了
@@ -78,11 +88,11 @@ public:
     //     }
     // }
 
-    
     // 析构函数
     ~IndexMaxHeap(){
         delete[] data;
         delete[] indexes;
+        delete[] reverses;
     }
 
     // 获取最大堆的大小
@@ -101,12 +111,16 @@ public:
         assert( count + 1 <= capacity);
         // 传入的i对用户而言，是从0开始地，但是对堆内部而言是从1开始地,所以需要判断i+1是否越界
         assert( i+1 >= 1 && i+1 <= capacity);
+        // 再插入一个新元素之前，还需要保证索引i所在的位置是没有元素地
+        assert(!contain(i));
         // 先加1，屏蔽外部数组和内部堆得差异
         i += 1;
         // 元素不用shiftUp
         data[i] = item;
-        // 插入新元素，元素数加1,之所以用++count而不用count++是因为数组下标从1开始
-        indexes[++count] = i;
+        // 插入新元素，元素数加1,是因为数组下标从1开始
+        count++;
+        indexes[count] = i;
+        reverses[i] = count;
         // 把新加入的元素的index(在indexes中的位置)向上浮动到合适位置
         shiftUp(count);
     }
@@ -119,14 +133,20 @@ public:
         Item max = data[indexes[1]];
         // 移出最大元素后，需要把最下面的元素移到上面去
         swap(indexes[1], indexes[count]);
+        // 这个元素相当于删除了，所以指向0，因为各个数组的下标都是从0开始地
+        reverses[indexes[count]] = 0;
         // 少了最后一个元素
         count--;
-        // 将换上去(到最上面了,根元素，下标为1)的最后一个元素下移
-        shiftDown(1);
+        if(count){
+            // 注意要重新设置根元素地索引
+            reverses[indexes[1]] = 1;
+            // 将换上去(到最上面了,根元素，下标为1)的最后一个元素下移
+            shiftDown(1);
+        }
         return max;
     }
 
-    // 弹出最大值的数组的下标(根节点的对象).元素一样会被删除
+    // 弹出最大值的数组的下标(根节点的对象)
     int popMaxIndex(){
         // 保证堆不为空
         assert( count >0);
@@ -135,12 +155,26 @@ public:
         int maxIndex = indexes[1] ;
         // 移出最大元素后，需要把最下面的元素移到上面去
         swap(indexes[1], indexes[count]);
+        // 这个元素相当于删除了，所以指向0，因为各个数组的下标都是从0开始地
+        reverses[indexes[count]] = 0;
         // 少了最后一个元素
         count--;
-        // 将换上去(到最上面了,根元素，下标为1)的最后一个元素下移
-        shiftDown(1);
+        if(count){
+            // 注意要重新设置根元素地索引
+            reverses[indexes[1]] = 1;
+            // 将换上去(到最上面了,根元素，下标为1)的最后一个元素下移
+            shiftDown(1);
+        }
         // 外面的用户数组索引是从0开始的，内部堆数组是从1开始地，所以需要减1
         return maxIndex-1;
+    }
+
+     // 判断堆中是否含有数组i位置的元素
+    bool contain(int i){
+        assert((i+1 >= 1)&&(i+1 <= capacity));
+        // i+1是因为外部数组和内存数组的起始坐标不同，前者从0开始，后者从1开始
+        // 不等于0是因为在shiftUP、shiftDown、Insert、popMax中都把不在队中的元素下标置为了0(堆中元素下标从1开始)
+        return reverses[i+1] != 0;
     }
 
     // 获取最大元素(堆中最上面的元素，即索引中为1的值)，但是并不删除
@@ -155,60 +189,69 @@ public:
         return indexes[1]-1;
     }
 
-
     Item getItem(int i){
-        // 先确定好范围合适不合适
-        assert( i + 1 >= 1 && i + 1 <= capacity );
+        // 先判断数组i对应的元素存在于堆中
+        assert(contain(i));
         // 获取指定下标的元素。无关大小
         return data[i+1];
     }
 
     // 更新数组中i下标位置的元素值
     void update(int i, Item newItem){
+        // 先判断数组i对应的元素存在于堆中
+        assert(contain(i));
         // 外部数组下标从0开始，内部数组下表从1开始，需要平衡下
         i++;
         data[i] = newItem;
-        // 找到indexes[j] = i, j表示data[i]在堆中的位置(结合堆得序号分配方式)，后面shiftUp和shiftDown都要用这个j
-        // 之后进行shiftUp(j),再shiftDown(j)即可
-        for( int j = 1; j <= count; j++){
-            if(indexes[j] == i){
-                // 因为难以知道上浮还是下沉，所以上下都做一下
-                shiftUp(j);
-                shiftDown(j);
-                return;
-            }
-        }
+        // 有了reverses之后更新数据就方便多了
+        int j = reverses[i];
+        shiftUp(j);
+        shiftDown(j);
     }
 
-    // 测试索引堆中的索引数组index
+    // 测试索引堆中的索引数组index和反向数组reverse
     // 注意:这个测试在向堆中插入元素以后, 不进行extract操作有效
-    bool testIndexes(){
+    bool testIndexesAndReverseIndexes(){
 
         int *copyIndexes = new int[count+1];
+        int *copyReverseIndexes = new int[count+1];
 
-        for( int i = 0 ; i <= count ; i ++ )
+        for( int i = 0 ; i <= count ; i ++ ){
             copyIndexes[i] = indexes[i];
+            copyReverseIndexes[i] = reverses[i];
+        }
 
-        copyIndexes[0] = 0;
+        copyIndexes[0] = copyReverseIndexes[0] = 0;
         std::sort(copyIndexes, copyIndexes + count + 1);
+        std::sort(copyReverseIndexes, copyReverseIndexes + count + 1);
 
-        // 在对索引堆中的索引进行排序后, 应该正好是1...count这count个索引
+        // 在对索引堆中的索引和反向索引进行排序后,
+        // 两个数组都应该正好是1...count这count个索引
         bool res = true;
         for( int i = 1 ; i <= count ; i ++ )
-            if( copyIndexes[i-1] + 1 != copyIndexes[i] ){
+            if( copyIndexes[i-1] + 1 != copyIndexes[i] ||
+                    copyReverseIndexes[i-1] + 1 != copyReverseIndexes[i] ){
                 res = false;
                 break;
             }
 
         delete[] copyIndexes;
+        delete[] copyReverseIndexes;
 
         if( !res ){
             cout<<"Error!"<<endl;
             return false;
         }
 
+        for(int i = 1 ; i <= count ; i ++ )
+            if( reverses[ indexes[i] ] != i ){
+                cout<<"Error 2"<<endl;
+                return false;
+            }
+
         return true;
     }
+
 // 下面是以树状进行打印的函数
 public:
     // 以树状打印整个堆结构
