d934a6e503c24cc01fa9ba341fbc2f8fe23cbad6
liang shan guang <1648266192@qq.com>
2018-04-25 00:26:09 +0800

    5.3:插入算法完成了

--- Part1Basic/JAVA/src/main/java/Chapter5BinarySearchTree/section2to3BasicAndInsert/BST.java
+++ Part1Basic/JAVA/src/main/java/Chapter5BinarySearchTree/section2to3BasicAndInsert/BST.java
@@ -1,10 +1,95 @@
 /***********************************************************
- * @Description : 
+ * @Description : 二分搜索树
  * @author      : 梁山广(Laing Shan Guang)
  * @date        : 2018/4/24 23:09
  * @email       : liangshanguang2@gmail.com
  ***********************************************************/
 package Chapter5BinarySearchTree.section2to3BasicAndInsert;
 
-public class BST implements Comparable<> {
+/**
+ * @param <Key>   作为树的索引关键词，必须是可比较地，所以需要继承Comparable
+ * @param <Value> 不需要可比较
+ */
+public class BST<Key extends Comparable<Key>, Value> {
+
+    /**
+     * 节点类
+     */
+    private class Node {
+        Key key;
+        Value value;
+        /**
+         * 左子节点
+         */
+        Node left;
+        /**
+         * 右子节点
+         */
+        Node right;
+
+        /**
+         * 构造函数
+         */
+        Node(Key key, Value value) {
+            // key是唯一地，不能重复
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    /**
+     * 根节点
+     */
+    private Node root;
+    /**
+     * 节点数
+     */
+    private int count;
+
+    public BST() {
+        root = null;
+        count = 0;
+    }
+
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    void insert(Key key, Value value) {
+        root = insert(root, key, value);
+    }
+
+    /**
+     * 向以node为根节点的二叉树搜索树种，插入节点(key,value)
+     * 返回插入新节点后的二叉搜索树的根
+     */
+    private Node insert(Node node, Key key, Value value) {
+        // 处理最基本事件，即可以退出递归的事件
+        if (node == null) {
+            // 递归到没有子树了，就在这个位置插入。
+            count++; // 元素数先+1
+            // 把当前要插入的节点作为根节点返回，一层层递归后会跟新树路径上的节点关系地
+            return new Node(key, value);
+        }
+        if (key.compareTo(node.key) < 0) {
+            // 小于根节点的键，往左边子树插入
+            node.left = insert(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            // 大于根节点的键，往右边子树插入
+            node.right = insert(node.right, key, value);
+        } else {
+            // 如果key相等就直接更新(注意二叉搜索树的键是唯一地)
+            node.value = value;
+        }
+
+        return node;
+    }
+
 }
