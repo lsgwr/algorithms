5f7c4b0fd0a6cf685b2e62e561b03ba84211c1d8
liang shan guang <1648266192@qq.com>
2018-04-26 00:10:10 +0800

    5.5:二叉搜索的前、中、后序遍历看完了

--- Part1Basic/CPP/05-Binary-Search-Tree/05-Traverse/BST.cpp
+++ Part1Basic/CPP/05-Binary-Search-Tree/05-Traverse/BST.cpp
@@ -1,9 +1,6 @@
 
 
 #include <iostream>
-#include "FileOps.h"
-#include <vector>
-#include <string>
 using namespace std;
 
 template<typename Key, typename Value>
@@ -187,40 +184,38 @@ private:
 };
 
 int main(void){
-        // 使用圣经作为我们的测试用例
-    string filename = "/Users/liangshanguang/Program/Algorithm/Play-with-Algorithms/05-Binary-Search-Tree/Course Code (C++)/04-Binary-Search-Tree-Search/bible.txt";
-    vector<string> words;
-    if( FileOps::readFile(filename, words) ) {
+    srand(time(NULL));
+    BST<int,int> bst = BST<int,int>();
 
-        cout << "There are totally " << words.size() << " words in " << filename << endl;
-        cout << endl;
-
-
-        // 测试 BST
-        time_t startTime = clock();
-
-        // 统计圣经中所有词的词频
-        // 注: 这个词频统计法相对简陋, 没有考虑很多文本处理中的特殊问题
-        // 在这里只做性能测试用
-        BST<string, int> bst = BST<string, int>();
-        for (vector<string>::iterator iter = words.begin(); iter != words.end(); iter++) {
-            int *res = bst.search(*iter);
-            if (res == NULL)
-                bst.insert(*iter, 1);
-            else
-                (*res)++;
-        }
-
-        // 输出圣经中god一词出现的频率
-        if(bst.contain("god"))
-            cout << "'god' : " << *bst.search("god") << endl;
-        else
-            cout << "No word 'god' in " << filename << endl;
-
-        time_t endTime = clock();
-
-        cout << "BST , time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s." << endl;
-        cout << endl;
+    // 取n个取值范围在[0...m)的随机整数放进二分搜索树中
+    int N = 10;
+    int M = 100;
+    for( int i = 0 ; i < N ; i ++ ){
+        int key = rand()%M;
+        // 为了后续测试方便,这里value值取和key值一样
+        int value = key;
+        cout<<key<<" ";
+        bst.insert(key,value);
     }
+    cout<<endl;
+
+    // 测试二分搜索树的size()
+    cout<<"size: "<<bst.size()<<endl<<endl;
+
+    // 测试二分搜索树的前序遍历 preOrder
+    cout<<"preOrder: "<<endl;
+    bst.preOrder();
+    cout<<endl;
+
+    // 测试二分搜索树的中序遍历 inOrder 中序排序可用于排序(升序)
+    cout<<"inOrder: "<<endl;
+    bst.inOrder();
+    cout<<endl;
+
+    // 测试二分搜索树的后序遍历 postOrder 后序遍历可用于释放二叉搜索树
+    cout<<"postOrder: "<<endl;
+    bst.postOrder();
+    cout<<endl;
+
     return 0;
 }
