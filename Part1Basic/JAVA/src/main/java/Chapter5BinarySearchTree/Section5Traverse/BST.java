1aa9734152b1da19c4d7ddf8ea35aafcb8c2536f
liang shan guang <1648266192@qq.com>
2018-04-26 00:06:17 +0800

    5.5:二叉搜索的前、中、后序遍历看完了

--- Part1Basic/JAVA/src/main/java/Chapter5BinarySearchTree/Section5Traverse/BST.java
+++ Part1Basic/JAVA/src/main/java/Chapter5BinarySearchTree/Section5Traverse/BST.java
@@ -209,45 +209,36 @@ public class BST<Key extends Comparable<Key>, Value> {
 
 
     public static void main(String[] args) {
-        int N = 1000000;
+        BST<Integer, Integer> bst = new BST<Integer, Integer>();
 
-        // 创建一个数组，包含[0...N)的所有元素
-        Integer[] arr = new Integer[N];
+        // 取n个取值范围在[0...m)的随机整数放进二分搜索树中
+        int N = 10;
+        int M = 100;
         for (int i = 0; i < N; i++) {
-            arr[i] = new Integer(i);
+            Integer key = (int) (Math.random() * M);
+            // 为了后续测试方便,这里value值取和key值一样
+            bst.insert(key, key);
+            System.out.print(key + " ");
         }
+        System.out.println();
 
-        // 打乱数组顺序
-        for (int i = 0; i < N; i++) {
-            int pos = (int) (Math.random() * (i + 1));
-            Integer t = arr[pos];
-            arr[pos] = arr[i];
-            arr[i] = t;
-        }
-        // 由于我们实现的二分搜索树不是平衡二叉树，
-        // 所以如果按照顺序插入一组数据，我们的二分搜索树会退化成为一个链表
-        // 平衡二叉树的实现，我们在这个课程中没有涉及，
-        // 有兴趣的同学可以查看资料自学诸如红黑树的实现
-        // 以后有机会，我会在别的课程里向大家介绍平衡二叉树的实现的：）
+        // 测试二分搜索树的size()
+        System.out.println("size: " + bst.size());
+        System.out.println();
 
+        // 测试二分搜索树的前序遍历 preOrder
+        System.out.println("preOrder: ");
+        bst.preOrder();
+        System.out.println();
 
-        // 我们测试用的的二分搜索树的键类型为Integer，值类型为String
-        // 键值的对应关系为每个整型对应代表这个整型的字符串
-        BST<Integer, String> bst = new BST<Integer, String>();
-        for (int i = 0; i < N; i++) {
-            bst.insert(new Integer(arr[i]), Integer.toString(arr[i]));
-        }
+        // 测试二分搜索树的中序遍历 inOrder 中序排序可用于排序(升序)
+        System.out.println("inOrder: ");
+        bst.inOrder();
+        System.out.println();
 
-        // 对[0...2*N)的所有整型测试在二分搜索树中查找
-        // 若i在[0...N)之间，则能查找到整型所对应的字符串
-        // 若i在[N...2*N)之间，则结果为null
-        for (int i = 0; i < 2 * N; i++) {
-            String res = bst.search(new Integer(i));
-            if (i < N) {
-                assert res.equals(Integer.toString(i));
-            } else {
-                assert res == null;
-            }
-        }
+        // 测试二分搜索树的后序遍历 postOrder  后序遍历可用于释放二叉搜索树
+        System.out.println("postOrder: ");
+        bst.postOrder();
+        System.out.println();
     }
 }
