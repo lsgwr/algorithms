35dcb5c2422ade32b37bd069c086f793476faddc
liang shan guang <1648266192@qq.com>
2018-04-30 13:00:23 +0800

    7-3:图的遍历，C++实现完成了

--- Part1Basic/CPP/07-Graph-Basics/03-Vertex-Adj-Iterator/DenseGraph.h
+++ Part1Basic/CPP/07-Graph-Basics/03-Vertex-Adj-Iterator/DenseGraph.h
@@ -1,14 +1,15 @@
 /***********************************************************
  * @Description : 稠密图--邻接矩阵
+ *                新增：遍历表的实现
  * @author      : 梁山广(Laing Shan Guang)
- * @date        : 2018/4/30 00:14
+ * @date        : 2018/4/30 12:09
  * @email       : liangshanguang2@gmail.com
  ***********************************************************/
 #ifndef DENSEGRAPH_H
 #define DENSEGRAPH_H
 
 #include <iostream>
-#include <vecto>
+#include <vector>
 #include <cassert>
 
 using namespace std;
@@ -31,7 +32,7 @@ public:
         this->directed = directed;
         for (int i = 0; i < vertices; ++i) {
             // 初始化邻接矩阵，每个边连接与否都初始化为false.注意下面的方式省去一层循环了
-            adj.push_back(vector<bool>(n, false));
+            adj.push_back(vector<bool>(vertices, false));
         }
     }
 
@@ -74,6 +75,47 @@ public:
         assert(w >= 0 && w < vertices);
         return adj[v][w];
     }
+
+    // 遍历表的迭代器
+    class adjIterator {
+    private:
+        // 图的引用
+        DenseGraph &graph;
+        // 指定的顶点
+        int v;
+        // 当前迭代到哪里
+        int index;
+    public:
+        adjIterator(DenseGraph &graph, int v) : graph(graph) {
+            assert(v >= 0 && v < graph.vertices);
+            this->v = v;
+            this->index = -1;
+        }
+
+        int begin() {
+            index = -1;
+            // 找到第一个等于true地，表示起点
+            return next();
+        }
+
+        int next() {
+            for (index += 1; index < graph.V(); ++index) {
+                // 遇到为true的才返回去
+                if (graph.adj[v][index] == true) {
+                    return index;
+                }
+            }
+            // 遍历不到返回-1
+            return -1;
+        }
+
+        // 是否已经遍历完了图G中与顶点V相连接的所有顶点
+        int end() {
+            // 索引打印顶点的个数
+            return index >= graph.V();
+        }
+
+    };
 };
 
 #endif //DENSEGRAPH_H
