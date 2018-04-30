0f3f71e0513e758c78de33086271e057691fcfa3
liang shan guang <1648266192@qq.com>
2018-04-30 20:45:06 +0800

    7-6:找出点之间的路径，Java

--- Part1Basic/JAVA/src/main/java/Chapter7GraphBasics/Section6FindAPath/Main.java
+++ Part1Basic/JAVA/src/main/java/Chapter7GraphBasics/Section6FindAPath/Main.java
@@ -8,19 +8,26 @@ package Chapter7GraphBasics.Section6FindAPath;
 
 public class Main {
     public static void main(String[] args) {
-        // TestG1.txt
-        String filename1 = "/Users/liangshanguang/Program/Algorithm/liuyubobobo-algorithms/Part1Basic/JAVA/src/main/java/Chapter7GraphBasics/Section5ConnectedConponents/graph1.txt";
+        // graph1.txt
+        System.out.println("*************************************稀疏图************************************");
+        String filename1 = "/Users/liangshanguang/Program/Algorithm/liuyubobobo-algorithms/Part1Basic/JAVA/src/main/java/Chapter7GraphBasics/Section6FindAPath/graph1.txt";
         SparseGraph g1 = new SparseGraph(13, false);
         ReadGraph readGraph1 = new ReadGraph(g1, filename1);
-        Components component1 = new Components(g1);
-        System.out.println("TestG1.txt, Components Count: " + component1.count());
         System.out.println();
+        Path path1 = new Path(g1, 0);
+        System.out.print("path of 0 to 3 :");
+        path1.showPath(3);
 
-        // TestG2.txt
-        String filename2 = "/Users/liangshanguang/Program/Algorithm/liuyubobobo-algorithms/Part1Basic/JAVA/src/main/java/Chapter7GraphBasics/Section5ConnectedConponents/graph2.txt";
-        SparseGraph g2 = new SparseGraph(6, false);
+
+        // graph2.txt
+        System.out.println("*************************************稠密图************************************");
+        String filename2 = "/Users/liangshanguang/Program/Algorithm/liuyubobobo-algorithms/Part1Basic/JAVA/src/main/java/Chapter7GraphBasics/Section6FindAPath/graph2.txt";
+        SparseGraph g2 = new SparseGraph(7, false);
         ReadGraph readGraph2 = new ReadGraph(g2, filename2);
-        Components component2 = new Components(g2);
-        System.out.println("TestG2.txt, Components Count: " + component2.count());
+        System.out.println();
+        Path path2 = new Path(g2, 0);
+        System.out.print("path of 0 to 6 :");
+        path2.showPath(6);
+
     }
 }
