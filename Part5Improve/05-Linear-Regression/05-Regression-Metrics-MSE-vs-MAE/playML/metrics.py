299f7e0d576f1a2510e986a105854c0ab5e5186b
liang shan guang <1648266192@qq.com>
2018-05-06 19:21:14 +0800

    05-06 R方的效果计算

--- Part5Improve/05-Linear-Regression/05-Regression-Metrics-MSE-vs-MAE/playML/metrics.py
+++ Part5Improve/05-Linear-Regression/05-Regression-Metrics-MSE-vs-MAE/playML/metrics.py
@@ -28,3 +28,8 @@ def mean_absolute_error(y_true, y_predict):
     """计算y_true和y_predict之间的MAE"""
 
     return np.sum(np.absolute(y_true - y_predict)) / len(y_true)
+
+
+def r2_score(y_true, y_predict):
+    """计算y_true和y_predict之间的R Square"""
+    return 1 - mean_squared_error(y_true, y_predict) / np.var(y_true)  # 模型的准确度.var 是方差值
