4f14d07d467b97ade4c8af7653c10e792d929939
liang shan guang <1648266192@qq.com>
2018-05-06 12:49:06 +0800

    05-03 简单线性回归的向量化

---
+++ Part5Improve/05-Linear-Regression/03-Simple-Linear-Regression-Implemention/playML/SimpleLinearRegression.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+class SimpleLinearRegression:
+
+    def __init__(self):
+        """初始化简单线性回归的模型"""
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        """根据训练数据里x_train，y_train训练简单线性回归的模型"""
+        assert x_train.ndim == 1, " 简单线性回归只处理有一个特性的训练数据"
+        assert len(x_train) == len(y_train), "x_train和y_train的大小必须相等"
+        numerator = 0.0  # 分子
+        denominator = 0.0  # 分母
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+        for x_i, y_i in zip(x_train, y_train):
+            numerator += (x_i - x_mean) * (y_i - y_mean)
+            denominator += (x_i - x_mean) ** 2
+        self.a_ = numerator / denominator
+        self.b_ = y_mean - self.a_ * x_mean
+        return self
+
+    def predict(self, x_predict):
+        """给定待预测数据集x_predict,返回表示x_predict的结果向量"""
+        assert x_predict.ndim == 1, "简单线性回归值只解决一个特征值的问题"
+        assert self.a_ is not None and self.b_ is not None, "must fit before predict"
+
+        return [self._predict(x) for x in x_predict]
+
+    def _predict(self, x):
+        """返回单个待测数据预测的结果值"""
+        return self.a_ * x + self.b_
+
+    def __repr__(self):
+        return "SimpleLinearRegression()"
