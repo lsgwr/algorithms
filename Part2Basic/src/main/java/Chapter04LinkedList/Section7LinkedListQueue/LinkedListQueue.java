b9f7911ccbe492a03bee4e24253534b9e28998cd
liang shan guang <1648266192@qq.com>
2018-05-16 08:15:22 +0800

    04-07 基于链表实现的队列，看完了

--- Part2Basic/src/main/java/Chapter04LinkedList/Section7LinkedListQueue/LinkedListQueue.java
+++ Part2Basic/src/main/java/Chapter04LinkedList/Section7LinkedListQueue/LinkedListQueue.java
@@ -8,7 +8,7 @@ package Chapter04LinkedList.Section7LinkedListQueue;
 
 import Chapter03StackAndQueues.Section5ArrayQueue.Queue;
 
-public class LinkedListQueue<Element> implements Queue {
+public class LinkedListQueue<Element> implements Queue<Element> {
 
     /**
      * 每个节点的封装
@@ -62,17 +62,72 @@ public class LinkedListQueue<Element> implements Queue {
     }
 
     @Override
-    public void enqueue(Object o) {
-
+    public void enqueue(Element element) {
+        // 从队列尾部入队
+        if (tail == null) {
+            // tail为null说明之前没有元素
+            tail = new Node(element);
+            // 第一个元素，head和tail应该都指向它
+            head = tail;
+        } else {
+            // 之前有元素，tail后移以为直线加入地Node
+            tail.next = new Node(element);
+            tail = tail.next;
+        }
+        size++;
     }
 
     @Override
-    public Object dequeue() {
-        return null;
+    public Element dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("队列不能为空！");
+        }
+        // 从队头删除元素(出队)
+        // 临时保存头结点
+        Node retNode = head;
+        // 头节点指针后移
+        head = head.next;
+        // 把原来的头结点置空
+        retNode.next = null;
+        if (head == null) {
+            // 如果头结点指针指向null,说明队列中没元素了,tail也该为null
+            tail = null;
+        }
+        size--;
+        return retNode.element;
     }
 
     @Override
-    public Object getFront() {
-        return null;
+    public Element getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("队列不能为空！");
+        }
+        return head.element;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Queue: front");
+        Node cur = head;
+        while (cur != null) {
+            sb.append(cur + "->");
+            cur = cur.next;
+        }
+        sb.append("NULL tail");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
     }
 }
