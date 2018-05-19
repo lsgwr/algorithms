173bb0db415fb261dcbfef5a48ccd6a358c5ead3
liang shan guang <1648266192@qq.com>
2018-05-19 23:17:02 +0800

    第11章， 参考《程序猿的内功修炼，学好算法与数据结构》第6章，完全一样，没必须再学一次，直接跳过了，拷过来一个最好的实现就够了

---
+++ Part1Basic/JAVA/src/main/java/Chapter6UnionFind/Section6FindOptimizedByPathCompression2/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * Created By Liang Shan Guang at 2018/5/19 23:05
+ * Description : 递归方法的路径压缩，因为递归，性能还会降低，所以上一个递归的更好
+ */
+package Chapter6UnionFind.Section6FindOptimizedByPathCompression2;
---
+++ Part2Basic/src/main/java/Chapter11UnionFind/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Created By Liang Shan Guang at 2018/5/19 23:10
+ * Description : 本章和《程序猿的内功修炼，学好算法与数据结构》
+ *               https://coding.imooc.com/learn/list/71.html
+ *               第6章完全重合，此处直接那里代码拿过来复用。
+ *               不在学本章的课了。选取地是Part1Basci 6-6-1
+ *               的路径压缩的第一个非递归实现的UnionFind,性能相较于
+ *               6-6-2实现的递归版路径压缩性能更高，更易理解
+ */
+package Chapter11UnionFind;
