f8427280a4e124b4124822563d74be3725fada17
liang shan guang <1648266192@qq.com>
2018-07-04 23:58:34 +0800

    新增2-3树的说明

---
+++ Part2Basic/src/main/java/Chapter13ReadBlackTree/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Created By Liang Shan Guang at 2018/7/4 23:43
+ * Description : 红黑树
+ * 性质：
+ * 1.  2-3树是一种绝对平衡的树.绝对平衡的含义：从根节点到任意一个叶子节点所经历的节点数都是相同的
+ * 2.  2-3树插入新节点的时候一定不会去空位置，去地是最后一个访问的叶子节点
+ */
+package Chapter13ReadBlackTree;
