1f9b067d7403a3df0553257bc382f0bb4489e17a
梁山广 <1648266192@qq.com>
2019-08-08 11:43:52 +0800

    Create Main.java

---
+++ Part2BasicGraph/src/main/java/Chapter04DFSInAction/Section10BiPartitionDetect/Main.java
@@ -0,0 +1,43 @@
+package Chapter04DFSInAction.Section10BiPartitionDetect;
+
+import Chapter02GraphExpress.Graph;
+import Chapter02GraphExpress.ReadGraph;
+
+/***********************************************************
+ * @note      : 
+ * @author    : l00379880 梁山广
+ * @version   : V1.0 at 2019/8/8 11:23
+ ***********************************************************/
+public class Main {
+
+    private static void showResult(String filePath) {
+        Graph graph = new Graph(7, false);
+        ReadGraph.init(graph, filePath);
+        GraphDFSBiPartition graphDFSBiPartition = new GraphDFSBiPartition(graph);
+        System.out.println(graphDFSBiPartition.isBiPartition());
+        System.out.println("当前图" + (graphDFSBiPartition.isBiPartition() ? "是" : "不是") + "二分图");
+    }
+
+    public static void main(String[] args) {
+        String filePath = "src/main/java/Chapter04DFSInAction/Section10BiPartitionDetect/graph.txt";
+        showResult(filePath);
+        filePath = "src/main/java/Chapter04DFSInAction/Section10BiPartitionDetect/graph2.txt";
+        showResult(filePath);
+        filePath = "src/main/java/Chapter04DFSInAction/Section10BiPartitionDetect/graph3.txt";
+        showResult(filePath);
+    }
+}
+
+/**
+ * 结果输出：
+ * <p>
+ * 顶点数V = 7, 边数E = 6
+ * true
+ * 当前图是二分图
+ * 顶点数V = 4, 边数E = 6
+ * false
+ * 当前图不是二分图
+ * 顶点数V = 4, 边数E = 4
+ * true
+ * 当前图是二分图
+ */
