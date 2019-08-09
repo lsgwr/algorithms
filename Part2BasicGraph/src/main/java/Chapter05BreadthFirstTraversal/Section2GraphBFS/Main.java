03c9dce10e74c48317fccd275f4bc97cd19bdaa8
liang shan guang <1648266192@qq.com>
2019-08-09 08:36:14 +0800

    开始测试 广度优先遍历

---
+++ Part2BasicGraph/src/main/java/Chapter05BreadthFirstTraversal/Section2GraphBFS/Main.java
@@ -0,0 +1,17 @@
+/***********************************************************
+ * @Description : 测试BFS
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2019-08-09 08:35
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package Chapter05BreadthFirstTraversal.Section2GraphBFS;
+
+import Chapter02GraphExpress.Graph;
+import Chapter02GraphExpress.ReadGraph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph(7, false);
+        ReadGraph.init(graph,);
+    }
+}
