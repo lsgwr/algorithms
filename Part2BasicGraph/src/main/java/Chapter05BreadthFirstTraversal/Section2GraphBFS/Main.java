86fd1e1b0c63e27ce584e137fdded801ee29a951
梁山广 <1648266192@qq.com>
2019-08-09 14:59:01 +0800

    Update Main.java

--- Part2BasicGraph/src/main/java/Chapter05BreadthFirstTraversal/Section2GraphBFS/Main.java
+++ Part2BasicGraph/src/main/java/Chapter05BreadthFirstTraversal/Section2GraphBFS/Main.java
@@ -11,7 +11,16 @@ import Chapter02GraphExpress.ReadGraph;
 
 public class Main {
     public static void main(String[] args) {
+        String filePath = "src/main/java/Chapter05BreadthFirstTraversal/Section2GraphBFS/graph.txt";
         Graph graph = new Graph(7, false);
-        ReadGraph.init(graph,);
+        ReadGraph.init(graph, filePath);
+        GraphBFS graphBFS = new GraphBFS(graph);
+        System.out.println("广度优先遍历顺序结果是：" + graphBFS.getOrder());
     }
 }
+
+/**
+ * 输出结果是：
+ * 顶点数V = 7, 边数E = 7
+ * 广度优先遍历顺序结果是：[0, 1, 2, 3, 4, 6, 5]
+ */
