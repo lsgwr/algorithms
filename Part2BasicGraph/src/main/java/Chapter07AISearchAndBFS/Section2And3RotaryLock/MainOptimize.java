318c42a168d3f67d331286685f820ebf6ecd369a
liang shan guang <1648266192@qq.com>
2019-08-11 23:51:18 +0800

    添加了路径获取的测试

--- Part2BasicGraph/src/main/java/Chapter07AISearchAndBFS/Section2And3RotaryLock/MainOptimize.java
+++ Part2BasicGraph/src/main/java/Chapter07AISearchAndBFS/Section2And3RotaryLock/MainOptimize.java
@@ -12,15 +12,19 @@ public class MainOptimize {
         String target = "0202";
         SolutionOptimize solution = new SolutionOptimize();
         System.out.println(solution.openLock(deadends, target));
+        System.out.println(solution.path());
 
         //
         String[] deadends2 = {"8888"};
         String target2 = "0009";
         System.out.println(solution.openLock(deadends2, target2));
+        System.out.println(solution.path());
 
         String[] deadends3 = {"8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"};
         String target3 = "888";
         System.out.println(solution.openLock(deadends3, target3));
+        // 没有路径，输出为null
+        System.out.println(solution.path());
     }
 }
 
@@ -28,6 +32,9 @@ public class MainOptimize {
  * 输出结果如下：
  * <p>
  * 6
+ * [0000, 1000, 1100, 1200, 1201, 1202, 0202]
  * 1
+ * [0000, 0009]
  * -1
+ * Exception in thread "main" java.lang.NullPointerException
  */
