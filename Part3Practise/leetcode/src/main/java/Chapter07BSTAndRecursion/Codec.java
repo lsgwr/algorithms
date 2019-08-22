67145f962c9f76fb79d47a0138c13e0ef9a5bbfc
梁山广 <1648266192@qq.com>
2019-08-22 18:48:19 +0800

    Update Codec.java

--- Part3Practise/leetcode/src/main/java/Chapter07BSTAndRecursion/Codec.java
+++ Part3Practise/leetcode/src/main/java/Chapter07BSTAndRecursion/Codec.java
@@ -1,5 +1,5 @@
 /***********************************************************
- * @Description : 297. 二叉树的序列化与反序列化
+ * @Description : 297. 二叉树的序列化与反序列化 实际是二叉树的层序遍历
  * @author      : 梁山广(Liang Shan Guang)
  * @date        : 2019/8/22 08:16
  * @email       : liangshanguang2@gmail.com
@@ -14,38 +14,41 @@ import java.util.List;
 public class Codec {
 
     public String serialize(TreeNode root) {      //用StringBuilder
-        StringBuilder res = ser_help(root, new StringBuilder());
+        StringBuilder res = serializeHelp(root, new StringBuilder());
         return res.toString();
     }
 
-    public StringBuilder ser_help(TreeNode root, StringBuilder str){
-        if(null == root){
+    public StringBuilder serializeHelp(TreeNode root, StringBuilder str) {
+        if (null == root) {
             str.append("null,");
             return str;
         }
         str.append(root.val);
         str.append(",");
-        str = ser_help(root.left, str);
-        str = ser_help(root.right, str);
+        str = serializeHelp(root.left, str);
+        str = serializeHelp(root.right, str);
         return str;
     }
 
     public TreeNode deserialize(String data) {
-        String[] str_word = data.split(",");
-        List<String> list_word = new LinkedList<String>(Arrays.asList(str_word));
-        return deser_help(list_word);
+        String[] wordsStr = data.split(",");
+        List<String> wordList= new LinkedList<>(Arrays.asList(wordsStr));
+        return deserializeHelp(wordList);
     }
 
-    public TreeNode deser_help(List<String> li){
-        if(li.get(0).equals("null")){
+    public TreeNode deserializeHelp(List<String> li) {
+        if (li.size() == 0) {
+            return null;
+        }
+
+        if (li.get(0).equals("null")) {
             li.remove(0);
             return null;
         }
         TreeNode res = new TreeNode(Integer.valueOf(li.get(0)));
         li.remove(0);
-        res.left = deser_help(li);
-        res.right = deser_help(li);
+        res.left = deserializeHelp(li);
+        res.right = deserializeHelp(li);
         return res;
     }
 }
-
