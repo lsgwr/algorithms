12b397f4b69c4cfcd859e26823a2749c91ce9a05
liang shan guang <1648266192@qq.com>
2019-08-23 23:49:30 +0800

    递归和记忆数组完成了

--- Part3Practise/leetcode/src/main/java/Chapter09DynamicAllocate/Section4HouseRobber/Solution1Recur.java
+++ Part3Practise/leetcode/src/main/java/Chapter09DynamicAllocate/Section4HouseRobber/Solution1Recur.java
@@ -1,18 +1,21 @@
 /***********************************************************
- * @Description : 198.House Robber 动态递归问题详解
+ * @Description : 198.House Robber 动态递归问题详解,使用记忆数组
  * @author      : 梁山广(Liang Shan Guang)
  * @date        : 2019/8/23 23:23
  * @email       : liangshanguang2@gmail.com
  ***********************************************************/
 package Chapter09DynamicAllocate.Section4HouseRobber;
 
-public class Solution {
+public class Solution1Recur {
+    public static int num;
+
     public int rob(int[] nums) {
         // 从起点0开始抢劫房子
         return tryRob(nums, 0);
     }
 
     private int tryRob(int[] nums, int index) {
+        num++;
         if (index >= nums.length) {
             return 0;
         }
@@ -25,8 +28,16 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 9, 3, 1};
-        int robMax = new Solution().rob(nums);
-        System.out.println(robMax);
+        int[] nums = {2, 7, 9, 3, 1, 4, 7, 9, 5};
+        int robMax = new Solution1Recur().rob(nums);
+        System.out.println("抢劫到的最多的钱是：" + robMax);
+        System.out.println("进入递归函数" + num + "次");
     }
 }
+
+/**
+ * 输出结果是：
+ * <p>
+ * 抢劫到的最多的钱是：24
+ * 进入递归函数89次
+ */
