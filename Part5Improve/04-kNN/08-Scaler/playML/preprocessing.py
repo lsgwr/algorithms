37c23d66b6c9af57a09c958ecbd11f20276b54c7
19920625lsg <1648266192@qq.com>
2019-11-10 15:31:14 +0800

    机器学习算法kNN的总结和更多的思考

--- Part5Improve/04-kNN/08-Scaler/playML/preprocessing.py
+++ Part5Improve/04-kNN/08-Scaler/playML/preprocessing.py
@@ -18,13 +18,13 @@ class StandardScaler:
         return self
 
     def transform(self, X):
-        """根据训练数据集获得数据的方差和均值"""
+        """根据训练数据集获得的数据的方差和均值进行归一化"""
         assert X.ndim == 2, "The dimension of X must be 2"
         # 必须是已经fit过地，mean_和scale_都不是None
         assert self.mean_ is not None and self.scale_ is not None, "must fit before transform"
         assert X.shape[1] == len(self.mean_), "the feature number of X must be equal to mean_ and std_"
-        resultX = np.empty(shape=X.shape, dtype=float)
-        # 遍历每一列
+        resultX = np.empty(shape=X.shape, dtype=float) # 初始化一个和X大小相等的空浮点型矩阵
+        # 遍历每一列进行归一化
         for col in range(X.shape[1]):
-            resultX = (X[:, col] - self.mean_[col]) / self.scale_;
+            resultX = (X[:, col] - self.mean_[col]) / self.scale_ # 均值方差归一化的核心原理
         return resultX
