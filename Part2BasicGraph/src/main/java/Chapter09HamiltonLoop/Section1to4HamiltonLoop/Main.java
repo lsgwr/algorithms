a64651b59dd6fd0daadde8b553832647b98a02c2
19920625lsg <1648266192@qq.com>
2019-12-20 23:44:50 +0800

    哈密尔顿回路测试代码改进

--- Part2BasicGraph/src/main/java/Chapter09HamiltonLoop/Section1to4HamiltonLoop/Main.java
+++ Part2BasicGraph/src/main/java/Chapter09HamiltonLoop/Section1to4HamiltonLoop/Main.java
@@ -18,7 +18,7 @@ public class Main {
         String filePath = "src/main/java/Chapter09HamiltonLoop/Section1to4HamiltonLoop/graph.txt";
         Graph graph = new Graph(false);
         ReadGraph.init(graph, filePath);
-        System.out.print("哈密尔顿路径详情为：");
+        System.out.print("哈密尔顿回路详情为：");
         List<Integer> path = new GraphDFSHamiltonLoop(graph).getLoop();
         System.out.println(path);
         System.out.println();
@@ -26,14 +26,15 @@ public class Main {
         filePath = "src/main/java/Chapter09HamiltonLoop/Section1to4HamiltonLoop/graph2.txt";
         graph = new Graph(false);
         ReadGraph.init(graph, filePath);
+        System.out.print("哈密尔顿回路详情为：");
         path = new GraphDFSHamiltonLoop(graph).getLoop();
         System.out.println(path);
     }
 }
 /**
  * 顶点数V = 4, 边数E = 5
- * 哈密尔顿路径详情为：[0, 2, 1, 3, 0]
+ * 哈密尔顿回路详情为：[0, 2, 1, 3, 0]
  *
  * 顶点数V = 20, 边数E = 30
- * [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 19, 18, 17, 16, 15, 14, 13, 0]
+ * 哈密尔顿回路详情为：[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 19, 18, 17, 16, 15, 14, 13, 0]
  */
