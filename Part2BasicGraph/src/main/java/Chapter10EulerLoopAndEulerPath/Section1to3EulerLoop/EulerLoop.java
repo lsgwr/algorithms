a3be1d58db67320f209a79535a01378dccae34c8
19920625lsg <1648266192@qq.com>
2019-12-21 20:14:56 +0800

    10.3 欧拉回路存在性检测代码实现

---
+++ Part2BasicGraph/src/main/java/Chapter10EulerLoopAndEulerPath/Section1to3EulerLoop/EulerLoop.java
@@ -0,0 +1,41 @@
+/***********************************************************
+ * @Description : 欧拉回路检测，满足如下3个条件
+ *                  1.无向
+ *                  2.联通
+ *                  3.每个点的度都是偶数
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2019/12/21 19:54
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package Chapter10EulerLoopAndEulerPath.Section1to3EulerLoop;
+
+import Chapter02GraphExpress.Graph;
+import Chapter04DFSInAction.Section1ConnectedComponents.GraphDFS4ConnectedComponents;
+
+public class EulerLoop {
+    private Graph graph;
+
+    public EulerLoop(Graph graph) {
+        this.graph = graph;
+    }
+
+    /**
+     * 判断图中是否有欧拉回路
+     */
+    public boolean hasEulerLoop() {
+        // 1.检测联通性
+        GraphDFS4ConnectedComponents cc = new GraphDFS4ConnectedComponents(graph);
+        if (cc.getConnectedComponentCount()>1){
+            // 多于一个联通分量肯定就不包含欧拉回路了
+            return false;
+        }
+        // 2.所有的点的度必须都是偶数
+        for (int v = 0; v < graph.V(); v++) {
+            if (graph.degree(v)%2==1){
+                // 如果有一个顶点的度是奇数，直接返回false
+                return false;
+            }
+        }
+        return true;
+    }
+}
