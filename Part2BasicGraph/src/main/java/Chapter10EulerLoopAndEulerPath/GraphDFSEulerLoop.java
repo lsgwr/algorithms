979bdee1978afd055bd887503ae1fd6aa41ed4c9
19920625lsg <1648266192@qq.com>
2019-12-22 11:50:42 +0800

    求欧拉回路的递归和非递归实现

--- Part2BasicGraph/src/main/java/Chapter10EulerLoopAndEulerPath/GraphDFSEulerLoop.java
+++ Part2BasicGraph/src/main/java/Chapter10EulerLoopAndEulerPath/GraphDFSEulerLoop.java
@@ -4,7 +4,7 @@
  *                  2.联通
  *                  3.每个点的度都是偶数
  * @author      : 梁山广(Liang Shan Guang)
- * @date        : 2019/12/21 19:54
+ * @date        : 2019/12/22 11:37
  * @email       : liangshanguang2@gmail.com
  ***********************************************************/
 package Chapter10EulerLoopAndEulerPath;
@@ -16,23 +16,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class GraphDFSNoRecursionEulerLoop {
+public class GraphDFSEulerLoop {
     private Graph graph;
-
     /**
      * 存储欧拉回路的结果
      */
     List<Integer> loop = new ArrayList<>();
 
-    public GraphDFSNoRecursionEulerLoop(Graph graph, int start) {
+    public GraphDFSEulerLoop(Graph graph, int START) {
         // 因为我们实现了Graph的深拷贝(clone函数)，所以赋值时是值传递，下面的修改不会影响原始的Graph
         this.graph = (Graph) graph.clone();
-        // 存储最终欧拉回路的list
-        if (!hasEulerLoop()) {
-            // 没有欧拉路径就返回空列表
-            return;
+        if (!hasEulerLoop()){
+            return ;
         }
-        dfs(start);
+        dfs(START);
     }
 
     /**
@@ -56,34 +53,21 @@ public class GraphDFSNoRecursionEulerLoop {
     }
 
     /**
-     * 基于非递归的DFS来实现欧拉回路的获取
+     * 基于递归的DFS来实现欧拉回路的获取
      *
-     * @param start DFS遍历开始的顶点
      * @return 欧拉回路的列表
      */
-    public void dfs(int start) {
-        // 下面两个栈是Hierholzer算法的核心用到的两个栈
-        Stack<Integer> curPath = new Stack<>();
-        // 开始遍历的起点
-        int vCur = start;
-        // 初始化加入下是为了能进while循环
-        curPath.push(vCur);
-        while (!curPath.isEmpty()) {
-            if (graph.degree(vCur) != 0) {
-                // 这里再加入一次0是为了最后一次pop后就进不了while循环了，所以要多个元素占着位置
-                curPath.push(vCur);
-                // 只要当前顶点还有临边就继续遍历
-                // 获取v的最小邻接点
-                int w = graph.adj(vCur).iterator().next();
-                graph.removeEdge(vCur, w);
-                vCur = w;
-            } else {
-                // 当前元素没有邻接点了，肯定是欧拉回路上的，所以加入loop
-                loop.add(vCur);
-                // 回退到上一个顶点
-                vCur = curPath.pop();
-            }
-        }
+    public void dfs(int v) {
+       if (graph.degree(v)==0){
+           loop.add(v);
+       }else {
+           while (graph.degree(v)!=0){
+               int w = graph.adj(v).iterator().next();
+               graph.removeEdge(v, w);
+               dfs(w);
+           }
+           loop.add(v);
+       }
     }
 
     public List<Integer> getLoop() {
--- Part2BasicGraph/src/main/java/Chapter10EulerLoopAndEulerPath/Main.java
+++ Part2BasicGraph/src/main/java/Chapter10EulerLoopAndEulerPath/Main.java
@@ -9,21 +9,44 @@ package Chapter10EulerLoopAndEulerPath;
 import Chapter02GraphExpress.Graph;
 import Chapter02GraphExpress.ReadGraph;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // 连通图,前序遍历
+        // 1.基于非递归的DFS实现求欧拉回路
         String filePath = "src/main/java/Chapter10EulerLoopAndEulerPath/graph.txt";
         Graph graph = new Graph(false);
         ReadGraph.init(graph, filePath);
-        System.out.println(new GraphDFSNoRecursionEulerLoop(graph).dfs());
+        List<Integer> loop1 = new GraphDFSNoRecursionEulerLoop(graph, 0).getLoop();
+        System.out.println(loop1);
 
         filePath = "src/main/java/Chapter10EulerLoopAndEulerPath/graph2.txt";
         graph = new Graph(false);
         ReadGraph.init(graph, filePath);
-        System.out.println(new GraphDFSNoRecursionEulerLoop(graph).dfs());
+        List<Integer> loop2 = new GraphDFSNoRecursionEulerLoop(graph, 0).getLoop();
+        System.out.println(loop2);
+        System.out.println();
+
+        // 2.基于递归的DFS实现求欧拉回路
+        filePath = "src/main/java/Chapter10EulerLoopAndEulerPath/graph.txt";
+        graph = new Graph(false);
+        ReadGraph.init(graph, filePath);
+        List<Integer> loop3 = new GraphDFSEulerLoop(graph, 0).getLoop();
+        System.out.println(loop3);
+
+        filePath = "src/main/java/Chapter10EulerLoopAndEulerPath/graph2.txt";
+        graph = new Graph(false);
+        ReadGraph.init(graph, filePath);
+        List<Integer> loop4 = new GraphDFSEulerLoop(graph, 0).getLoop();
+        System.out.println(loop4);
     }
 }
 /**
+ * 顶点数V = 5, 边数E = 6
+ * [0, 2, 4, 3, 2, 1, 0]
+ * 顶点数V = 11, 边数E = 15
+ * [0, 3, 4, 6, 7, 9, 10, 8, 7, 5, 4, 1, 5, 2, 1, 0]
+ *
  * 顶点数V = 5, 边数E = 6
  * [0, 2, 4, 3, 2, 1, 0]
  * 顶点数V = 11, 边数E = 15
