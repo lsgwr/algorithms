f618abf87e744bbb4d6b0a03561cb7913b893796
19920625lsg <1648266192@qq.com>
2019-12-22 20:41:13 +0800

    开始Kruskal算法啦

---
+++ Part2BasicGraph/src/main/java/Chapter11WeightedGraphAndMinimumSpanningTree/Section3to5Kruskal/WeightedEdge.java
@@ -0,0 +1,38 @@
+/***********************************************************
+ * @Description : 无向有权图的边的表示
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2019/12/22 20:13
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package Chapter11WeightedGraphAndMinimumSpanningTree.Section3to5Kruskal;
+
+public class WeightedEdge implements Comparable<WeightedEdge> {
+    /**
+     * 顶点
+     */
+    private int v;
+    /**
+     * 顶点
+     */
+    private int w;
+    /**
+     * 边v-w的权重
+     */
+    private int weight;
+
+    public WeightedEdge(int v, int w, int weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d-%d: %d)", v, w, weight);
+    }
+
+    @Override
+    public int compareTo(WeightedEdge that) {
+        return this.weight - that.weight;
+    }
+}
