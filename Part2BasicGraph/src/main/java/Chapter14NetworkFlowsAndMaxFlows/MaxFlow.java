b152e3ec5562f0a217b666d094548eb2f1b20a68
19920625lsg <1648266192@qq.com>
2019-12-27 08:10:29 +0800

    14.5 只差更新残量图了

--- Part2BasicGraph/src/main/java/Chapter14NetworkFlowsAndMaxFlows/MaxFlow.java
+++ Part2BasicGraph/src/main/java/Chapter14NetworkFlowsAndMaxFlows/MaxFlow.java
@@ -8,7 +8,7 @@ package Chapter14NetworkFlowsAndMaxFlows;
 
 import Chapter11WeightedGraphAndMinimumSpanningTree.Section1To2WeightedGraph.WeightedGraph;
 
-import java.util.List;
+import java.util.*;
 
 public class MaxFlow {
     /**
@@ -33,6 +33,16 @@ public class MaxFlow {
      */
     private int maxFlow;
 
+    /**
+     * 顶点的访问情况的数组
+     */
+    private boolean[] visited;
+
+    /**
+     * 记录上一个访问顶点
+     */
+    private int[] pre;
+
     public MaxFlow(WeightedGraph networkGraph, int source, int target) {
         if (!networkGraph.isDirected()) {
             throw new IllegalArgumentException("最大流算法只支持有向图！");
@@ -51,6 +61,7 @@ public class MaxFlow {
 
         this.residualGraph = new WeightedGraph(networkGraph.V(), true);
 
+        edmondsKarp();
     }
 
     /**
@@ -74,17 +85,95 @@ public class MaxFlow {
             }
             // 增广路径上的流量f
             int flow = Integer.MAX_VALUE;
-            // Todo:计算增广路径的权值最小值，作为增广路径的流量
+            // 计算增广路径的权值最小值，作为增广路径的流量
+            for (int i = 1; i < augPath.size(); i++) {
+                int v = augPath.get(i-1);
+                int w = augPath.get(i);
+                // 不断更新本次增广路径上的最小流量值
+                flow = Math.min(flow, residualGraph.getWeight(v, w));
+            }
             // 不断累积每条增广路径的流量，最终找完所有的增广路径就得到了最终的网络最大流
             maxFlow += flow;
+            // Todo:根据增广路径更新残量图residualGraph
         }
     }
 
     /**
-     * 获取增广路径上所有顶点的集合
+     * 获取增广路径上所有顶点的集合，实际就是广度优先遍历
      */
     private List<Integer> getAugmentingPath() {
-        // Todo:获取增广路径
-        return null;
+        // 残量网络进行bfs需要的变量，每次获取增广路径都要进行一次bfs，所以每次getAugmentingPath()都需要初始化visited和pre数组
+        this.visited = new boolean[residualGraph.V()];
+        // 给存储单源路径的数组赋值
+        pre = new int[residualGraph.V()];
+        Arrays.fill(pre, -1);
+
+        // 开始bfs获取增广路径
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(source);
+        visited[source] = true;
+        // 起点的上一个节点可以认为是自己
+        pre[source] = source;
+        while (!queue.isEmpty()) {
+            int v = queue.remove();
+            if (v==target){
+                // 到达汇点就可以退出了
+                break;
+            }
+            for (int w : residualGraph.adj(v)) {
+                // 顶点没被访问而且边v->w的权值不为0
+                if (!visited[w] && residualGraph.getWeight(v, w) > 0) {
+                    queue.add(w);
+                    visited[w] = true;
+                    pre[w] = v;
+                }
+            }
+        }
+        List<Integer> augPath = new ArrayList<>();
+        if (!visited[target]){
+            return augPath;
+        }else {
+            // 找到了源点source到汇点target增广路径
+            augPath = (List<Integer>) path(target);
+        }
+        return augPath;
+    }
+
+    /**
+     * 判断图的遍历起点是否和target点连通，实际只需要看下visit[v]是否为true即可，为true表示在一个连通分量上，肯定是连通地
+     *
+     * @param target 目标定点索引
+     * @return 判断图的遍历起点是否和target点连通
+     */
+    public boolean isSourceConnectedTo(int target) {
+        networkGraph.validateVertex(target);
+        return visited[target];
+    }
+
+    /**
+     * 找到起点source到目标定点target的路径
+     *
+     * @param target 目标定点
+     * @return 可迭代的对象，一般是集合，用于存储source到target的完整路径
+     */
+    public Iterable<Integer> path(int target) {
+        List<Integer> pathList = new ArrayList<>();
+        // source到target有路径才进行路径查找
+        if (isSourceConnectedTo(target)) {
+            // 用pre数组从target一直找到source点，记录下中间经过的所有点，就是要求的单源路径
+            int current = target;
+            while (current != source) {
+                pathList.add(current);
+                current = pre[current];
+            }
+            // 起点要加上
+            pathList.add(source);
+            // 因为是从source到target的路径，所以要颠倒下
+            Collections.reverse(pathList);
+            return pathList;
+        } else {
+            // 没有路径就直接返回空集合
+            return pathList;
+        }
     }
 }
