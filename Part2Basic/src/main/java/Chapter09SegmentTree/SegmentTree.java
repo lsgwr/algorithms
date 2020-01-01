e7d29b0a93b5bf96ca752b9a00a17b7505645d7b
19920625lsg <1648266192@qq.com>
2020-01-01 20:31:55 +0800

    线段树简化

--- Part2Basic/src/main/java/Chapter09SegmentTree/SegmentTree.java
+++ Part2Basic/src/main/java/Chapter09SegmentTree/SegmentTree.java
@@ -1,5 +1,5 @@
 /***********************************************************
- * @Description : 线段树
+ * @Description : 线段树(也称为区间树)
  * @author      : 梁山广(Laing Shan Guang)
  * @date        : 2018/5/18 23:02
  * @email       : liangshanguang2@gmail.com
@@ -8,17 +8,17 @@ package Chapter09SegmentTree;
 
 import java.util.Arrays;
 
-public class SegmentTree<Element> {
-    private Element[] tree;
-    private Element[] data;
-    private Merger<Element> merger;
+public class SegmentTree<E> {
+    private E[] tree;
+    private E[] data;
+    private Merger<E> merger;
 
-    public SegmentTree(Element[] arr, Merger<Element> merger) {
-        this.data = (Element[]) new Object[arr.length];
+    public SegmentTree(E[] arr, Merger<E> merger) {
+        this.data = (E[]) new Object[arr.length];
         this.merger = merger;
         // 数组复制
         System.arraycopy(arr, 0, data, 0, arr.length);
-        tree = (Element[]) new Object[4 * arr.length];
+        tree = (E[]) new Object[4 * arr.length];
         buildSegmentTree(0, 0, data.length - 1);
     }
 
@@ -53,7 +53,7 @@ public class SegmentTree<Element> {
      * @param queryR 区间右边界
      * @return 返回得到的merge结果
      */
-    public Element query(int queryL, int queryR) {
+    public E query(int queryL, int queryR) {
         assert queryL <= queryR;
         if (queryL < 0 || queryL >= data.length || queryR < 0 || queryR >= data.length) {
             throw new IllegalArgumentException("Index is illegal");
@@ -71,7 +71,7 @@ public class SegmentTree<Element> {
      * @param queryR    要查询的右边界
      * @return 查询并Merge后的结果
      */
-    private Element query(int treeIndex, int l, int r, int queryL, int queryR) {
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
         // 当区间完全重合的时候，直接返回根节点的值就行
         if (l == queryL && r == queryR) {
             return tree[treeIndex];
@@ -89,26 +89,26 @@ public class SegmentTree<Element> {
         }
 
         // 在左右两侧各有一部分,要分开查，然后合并
-        Element leftResult = query(leftTreeIndex, l, mid, queryL, mid);
-        Element rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
         return merger.merge(leftResult, rightResult);
     }
 
     /**
      * 更新指定位置的元素
      */
-    public void update(int index, Element element) {
+    public void update(int index, E e) {
         if (index < 0 || index >= data.length) {
             throw new IllegalArgumentException("Index is illegal");
         }
-        data[index] = element;
+        data[index] = e;
         // 下面是更新线段树
-        update(0, 0, data.length - 1, index, element);
+        update(0, 0, data.length - 1, index, e);
     }
 
-    private void update(int treeIndex, int l, int r, int index, Element element) {
+    private void update(int treeIndex, int l, int r, int index, E e) {
         if (l == r) {
-            tree[treeIndex] = element;
+            tree[treeIndex] = e;
             return;
         }
         // 获取左孩子的索引
@@ -119,16 +119,16 @@ public class SegmentTree<Element> {
         // 一定是大于等于，别只写成大于哈
         if (index >= mid + 1) {
             // 右子树中查找
-            update(rightTreeIndex, mid + 1, r, index, element);
+            update(rightTreeIndex, mid + 1, r, index, e);
         } else {
             // 左子树中查找
-            update(leftTreeIndex, l, mid, index, element);
+            update(leftTreeIndex, l, mid, index, e);
         }
 
         tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
     }
 
-    public Element get(int index) {
+    public E get(int index) {
         if (index < 0 || index >= data.length) {
             throw new IllegalArgumentException("Index is illegal");
         }
--- Part2Basic/src/main/java/Chapter09SegmentTree/package-info.java
+++ Part2Basic/src/main/java/Chapter09SegmentTree/package-info.java
@@ -1,5 +1,5 @@
 /*
  * Created By Liang Shan Guang at 2018/5/18 22:59
- * Description : 线段树
+ * Description : 线段树，也称为区间树
  */
 package Chapter09SegmentTree;
