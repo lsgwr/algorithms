08f3dcbd282174a5aa182b58572318d605bc585d
19920625lsg <1648266192@qq.com>
2020-01-05 20:54:48 +0800

    第14章 哈希表看完

---
+++ Part2Basic/src/main/java/Chapter14HashTable/Section7PerfectHashTable/HashTable.java
@@ -0,0 +1,103 @@
+/***********************************************************
+ * @Description : 最完整的HashTable实现
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2020/1/5 20:54
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package Chapter14HashTable.Section7PerfectHashTable;
+
+import java.util.TreeMap;
+
+public class HashTable<K extends Comparable<K>, V> {
+
+    private final int[] capacity
+            = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593,
+            49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469,
+            12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741};
+
+    private static final int upperTol = 10;
+    private static final int lowerTol = 2;
+    private int capacityIndex = 0;
+
+    private TreeMap<K, V>[] hashtable;
+    private int size;
+    private int M;
+
+    public HashTable(){
+        this.M = capacity[capacityIndex];
+        size = 0;
+        hashtable = new TreeMap[M];
+        for(int i = 0 ; i < M ; i ++)
+            hashtable[i] = new TreeMap<>();
+    }
+
+    private int hash(K key){
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void add(K key, V value){
+        TreeMap<K, V> map = hashtable[hash(key)];
+        if(map.containsKey(key))
+            map.put(key, value);
+        else{
+            map.put(key, value);
+            size ++;
+
+            if(size >= upperTol * M && capacityIndex + 1 < capacity.length){
+                capacityIndex ++;
+                resize(capacity[capacityIndex]);
+            }
+        }
+    }
+
+    public V remove(K key){
+        V ret = null;
+        TreeMap<K, V> map = hashtable[hash(key)];
+        if(map.containsKey(key)){
+            ret = map.remove(key);
+            size --;
+
+            if(size < lowerTol * M && capacityIndex - 1 >= 0){
+                capacityIndex --;
+                resize(capacity[capacityIndex]);
+            }
+        }
+        return ret;
+    }
+
+    public void set(K key, V value){
+        TreeMap<K, V> map = hashtable[hash(key)];
+        if(!map.containsKey(key))
+            throw new IllegalArgumentException(key + " doesn't exist!");
+
+        map.put(key, value);
+    }
+
+    public boolean contains(K key){
+        return hashtable[hash(key)].containsKey(key);
+    }
+
+    public V get(K key){
+        return hashtable[hash(key)].get(key);
+    }
+
+    private void resize(int newM){
+        TreeMap<K, V>[] newHashTable = new TreeMap[newM];
+        for(int i = 0 ; i < newM ; i ++)
+            newHashTable[i] = new TreeMap<>();
+
+        int oldM = M;
+        this.M = newM;
+        for(int i = 0 ; i < oldM ; i ++){
+            TreeMap<K, V> map = hashtable[i];
+            for(K key: map.keySet())
+                newHashTable[hash(key)].put(key, map.get(key));
+        }
+
+        this.hashtable = newHashTable;
+    }
+}
