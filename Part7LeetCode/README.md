75da459e4576fb89fa7d804096cf86120b374267
19920625lsg <1648266192@qq.com>
2020-01-05 23:47:54 +0800

    开始刷LeetCode啦

---
+++ Part7LeetCode/README.md
@@ -0,0 +1,8 @@
+# LeetCode刷题
+
+> 参考资料如下，把里面的151道题都好好做一遍，pdf下载在当前文件夹啦
+
+## 算法珠玑
+
++ [github: soulmachine/leetcode](https://github.com/soulmachine/leetcode)
++ [gitbook: soulmachine/algorithm-essentials/details](https://legacy.gitbook.com/book/soulmachine/algorithm-essentials/details)
---
+++ Part7LeetCode/src/main/java/线性表/单链表/T206_反转链表/Solution.java
@@ -0,0 +1,34 @@
+/***********************************************************
+ * @Description : https://leetcode-cn.com/problems/reverse-linked-list/
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2020/1/5 23:46
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package 线性表.单链表.T206_反转链表;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Jello");
+    }
+}
