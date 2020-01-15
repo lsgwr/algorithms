ea5cff5d41181287d1c3cf866c49b64ca6ee0d5e
19920625lsg <1648266192@qq.com>
2020-01-16 00:21:21 +0800

    LeetCode76号问题解决啦~~

--- Part3Practise/src/main/java/Chapter03/LeetCode76/Solution.java
+++ Part3Practise/src/main/java/Chapter03/LeetCode76/Solution.java
@@ -8,11 +8,13 @@ package Chapter03.LeetCode76;
 
 class Solution {
     public String minWindow(String s, String p) {
-        if ("".equals(s) || "".equals(p) || s.length() < p.length()) {
+        int sLen = s.length();
+        int pLen = p.length();
+        if ("".equals(s) || "".equals(p) || sLen < pLen) {
             return "";
         }
         // s="ab" p="A
-        if (p.length() == 1) {
+        if (pLen == 1) {
             if (s.contains(p)) {
                 return p;
             } else {
@@ -21,22 +23,23 @@ class Solution {
         }
         int[] freq = new int[256];
         int[] freqTmp = new int[256];
+
         // 更新在p中的字符
-        for (int i = 0; i < p.length(); i++) {
+        for (int i = 0; i < pLen; i++) {
             freq[p.charAt(i)] += 1;
             // 初始化窗口内的字符频率
             freqTmp[s.charAt(i)] += 1;
         }
         int l = 0;
-        int r = p.length();
+        int r = pLen;
         int lMin = 0;
-        int rMin = p.length();
+        int rMin = pLen;
         // 找最小时，初始应为最大值
         int min = Integer.MAX_VALUE;
         // 右边界还没滑动到末尾时就一直往后滑动
-        while (r <= s.length()) {
+        while (r <= sLen) {
             boolean find = true;
-            for (int i = 0; i < p.length(); i++) {
+            for (int i = 0; i < pLen; i++) {
                 if (freqTmp[p.charAt(i)] == 0) {
                     // 有一个p中的字母在s中找不到，直接跳出
                     find = false;
@@ -61,14 +64,14 @@ class Solution {
 
             if (freq[s.charAt(l)] == 0) {
                 // 最左边的字母不在p中，此时窗口宽度必须必p的长度大，这样左边的l才可以右移
-                if (r - l > p.length()) {
+                if (r - l > pLen) {
                     // 在维持窗口宽度的前提下，左边界l可以向右移动地
                     freqTmp[s.charAt(l)] -= 1;
                     l++;
                 } else {
                     // 长度不够，r必须先往右侧移动
                     // 必须放在这里~~否则会提前退出地
-                    if (r + 1 > s.length()) {
+                    if (r + 1 > sLen) {
                         break;
                     }
                     freqTmp[s.charAt(r)] += 1;
@@ -82,7 +85,7 @@ class Solution {
                     l++;
                 } else {
                     // 必须放在这里~~否则会提前退出地
-                    if (r + 1 > s.length()) {
+                    if (r + 1 > sLen) {
                         break;
                     }
                     // 其他情况下只能右侧元素往右移动，并更新频率
@@ -108,9 +111,9 @@ class Solution {
      * S = "aabAA", T = "BB"
      */
     public static void main(String[] args) {
-        String S = "abc";
+        String S = "aabAA";
         // cba、acd都测测
-        String T = "cba";
+        String T = "BB";
         System.out.println(new Solution().minWindow(S, T));
     }
 }
