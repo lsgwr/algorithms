87985ba9630768681367effea79f66edf8e62a56
19920625lsg <1648266192@qq.com>
2020-01-16 00:13:56 +0800

    终于通过了全部用例，但是超时了~~

--- Part3Practise/src/main/java/Chapter03/LeetCode76/Solution.java
+++ Part3Practise/src/main/java/Chapter03/LeetCode76/Solution.java
@@ -12,10 +12,10 @@ class Solution {
             return "";
         }
         // s="ab" p="A
-        if (p.length()==1){
-            if (s.contains(p)){
+        if (p.length() == 1) {
+            if (s.contains(p)) {
                 return p;
-            }else {
+            } else {
                 return "";
             }
         }
@@ -41,9 +41,9 @@ class Solution {
                     // 有一个p中的字母在s中找不到，直接跳出
                     find = false;
                     break;
-                }else {
+                } else {
                     // 能找到但是频率小，出现频率大于等于可以
-                    if (freqTmp[p.charAt(i)] < freq[p.charAt(i)]){
+                    if (freqTmp[p.charAt(i)] < freq[p.charAt(i)]) {
                         find = false;
                         break;
                     }
@@ -60,9 +60,21 @@ class Solution {
             }
 
             if (freq[s.charAt(l)] == 0) {
-                // 最左边的字母不在p中，左边界l一定是可以向右移动地，需要更新freqTmp
-                freqTmp[s.charAt(l)] -= 1;
-                l++;
+                // 最左边的字母不在p中，此时窗口宽度必须必p的长度大，这样左边的l才可以右移
+                if (r - l > p.length()) {
+                    // 在维持窗口宽度的前提下，左边界l可以向右移动地
+                    freqTmp[s.charAt(l)] -= 1;
+                    l++;
+                } else {
+                    // 长度不够，r必须先往右侧移动
+                    // 必须放在这里~~否则会提前退出地
+                    if (r + 1 > s.length()) {
+                        break;
+                    }
+                    freqTmp[s.charAt(r)] += 1;
+                    r++;
+                }
+
             } else {
                 // 最左边的元素在p中，同时它的频率大于在p中的频率了，左边界l一定是可以向右移动地，需要更新freqTmp
                 if (freqTmp[s.charAt(l)] > freq[s.charAt(l)]) {
@@ -79,9 +91,9 @@ class Solution {
                 }
             }
         }
-        if (min==Integer.MAX_VALUE){
+        if (min == Integer.MAX_VALUE) {
             return "";
-        }else {
+        } else {
             return s.substring(lMin, rMin);
         }
     }
@@ -93,12 +105,12 @@ class Solution {
      * S = "abc", T = "cba"  ==> abc
      * S = "bbaa", T = "aba"  ==>baa
      * S = "aaaaaaaaaaaabbbbbcdd", T = "abcdd"  ==> abbbbbcdd
-     *
+     * S = "aabAA", T = "BB"
      */
     public static void main(String[] args) {
-        String S = "aaaaaaaaaaaabbbbbcdd";
+        String S = "abc";
         // cba、acd都测测
-        String T = "abcdd";
+        String T = "cba";
         System.out.println(new Solution().minWindow(S, T));
     }
 }
