3324fa6a3a2f6c03d1b4b076005ef2b33d9bcb11
19920625lsg <1648266192@qq.com>
2020-01-17 00:12:30 +0800

    Four Sum的问题解决啦~~~

--- Part3Practise/src/main/java/Chapter03/FourSum/Solution.java
+++ Part3Practise/src/main/java/Chapter03/FourSum/Solution.java
@@ -1,5 +1,5 @@
 /***********************************************************
- * @Description : LeetCode第18号问题：三数之和
+ * @Description : LeetCode第18号问题：四数之和
  * https://leetcode-cn.com/problems/4sum/
  * @author      : 梁山广(Liang Shan Guang)
  * @date        : 2020/1/16 23:46
@@ -15,7 +15,7 @@ class Solution {
      *
      * @param nums   要检索的数组
      * @param target 要求的目标值
-     * @return 符合条件的下标对
+     * @return 符合条件的元素列表
      */
     public List<List<Integer>> twoSum(int[] nums, int target) {
         List<List<Integer>> resultList = new ArrayList<>();
@@ -26,7 +26,7 @@ class Solution {
             Integer index = mapValIndex.get(target - nums[i]);
             if (index != null) {
                 // 找到了一个满足条件地
-                resultList.add(new ArrayList<>(Arrays.asList(index, i)));
+                resultList.add(new ArrayList<>(Arrays.asList(nums[index], nums[i])));
             }
             // 加入当前节点
             mapValIndex.put(nums[i], i);
@@ -51,9 +51,9 @@ class Solution {
         for (int i = 2; i < len; i++) {
             // 时间优化
             if (nums[i] == nums[i - 1] && nums[i] == nums[i - 2]) {
-                if (nums[i] == 0) {
-                    // 把三个0加进去
-                    result.add(new ArrayList<>(Arrays.asList(0, 0, 0)));
+                if (nums[i] + nums[i - 1] + nums[i - 2] == target) {
+                    // 把三个加进去
+                    result.add(new ArrayList<>(Arrays.asList(nums[i], nums[i - 1], nums[i - 2])));
                 }
                 // 三个连续相等地，第3个开始就不用看了
                 continue;
@@ -64,7 +64,7 @@ class Solution {
                 // 找到的两个索引不能和当前索引相等
                 if (twoSumResult.size() == 2) {
                     // 这个是set，会自动去重
-                    result.add(Arrays.asList(nums[twoSumResult.get(0)], nums[twoSumResult.get(1)], nums[i]));
+                    result.add(Arrays.asList(twoSumResult.get(0), twoSumResult.get(1), nums[i]));
                 }
             }
         }
@@ -73,8 +73,34 @@ class Solution {
 
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        // Todo:
-        return null;
+        // 这个排序很重要！！可能省掉下面大量的Collections.sort()
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new HashSet<>();
+        if (nums.length < 4) {
+            return new ArrayList<>();
+        }
+        int len = nums.length;
+        for (int i = 3; i < len; i++) {
+            // 时间优化
+            if (nums[i] == nums[i - 1] && nums[i] == nums[i - 2] && nums[i] == nums[i - 3]) {
+                if (nums[i] + nums[i - 1] + nums[i - 2] + nums[i - 3] == target) {
+                    // 把三个加进去
+                    result.add(new ArrayList<>(Arrays.asList(nums[i], nums[i - 1], nums[i - 2], nums[i - 3])));
+                }
+                // 四个连续相等地，第4个开始就不用看了
+                continue;
+            }
+            // 注意twoSum返回地是nums的索引下标
+            List<List<Integer>> threeSumResultList = threeSum(Arrays.copyOfRange(nums, 0, i), target - nums[i]);
+            for (List<Integer> threeSumResult : threeSumResultList) {
+                // 找到的两个索引不能和当前索引相等
+                if (threeSumResult.size() == 3) {
+                    // 这个是set，会自动去重
+                    result.add(Arrays.asList(threeSumResult.get(0), threeSumResult.get(1), threeSumResult.get(2), nums[i]));
+                }
+            }
+        }
+        return new ArrayList<>(result);
     }
 
     /**
@@ -83,8 +109,9 @@ class Solution {
      * {-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}  ==> [[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]]
      */
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> lists = new Solution().threeSum(nums, 0);
+        int[] nums = {0, 0, 0, 0};
+        int target=1;
+        List<List<Integer>> lists = new Solution().fourSum(nums, target);
         System.out.println(lists);
     }
 }
