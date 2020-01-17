f400392544fbc1578dfb5b78cb73b1e913ba8d8d
19920625lsg <1648266192@qq.com>
2020-01-18 00:46:23 +0800

    206和92号问题：反转列表

---
+++ Part3Practise/src/main/java/Chapter05LinkedList/ListNode.java
@@ -0,0 +1,16 @@
+/***********************************************************
+ * @Description : 链表的节点
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2020/1/17 22:13
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package Chapter05LinkedList;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
