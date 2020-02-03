172780e422315eaf382da15c22ddc49ed47e11c9
19920625lsg <1648266192@qq.com>
2020-02-03 18:47:01 +0800

    重命名

--- Part9Nowcoder/src/main/java/第2章_链表问题/第03题_删除链表的中间节点和b分之a处的节点/删除b分之a处的节点/Solution.java
+++ Part9Nowcoder/src/main/java/第2章_链表问题/第03题_删除链表的中间节点和b分之a处的节点/删除b分之a处的节点/Solution.java
@@ -4,7 +4,7 @@
  * @date        : 2020/2/3 18:32
  * @email       : liangshanguang2@gmail.com
  ***********************************************************/
-package 第2章_链表问题.第3题_删除链表的中间节点和b分之a处的节点.删除b分之a处的节点;
+package 第2章_链表问题.第03题_删除链表的中间节点和b分之a处的节点.删除b分之a处的节点;
 
 import 第2章_链表问题.LinkedListTool;
 import 第2章_链表问题.ListNode;
--- Part9Nowcoder/src/main/java/第2章_链表问题/第03题_删除链表的中间节点和b分之a处的节点/删除中间节点/Solution.java
+++ Part9Nowcoder/src/main/java/第2章_链表问题/第03题_删除链表的中间节点和b分之a处的节点/删除中间节点/Solution.java
@@ -5,7 +5,7 @@
  * @date        : 2020/2/3 17:46
  * @email       : liangshanguang2@gmail.com
  ***********************************************************/
-package 第2章_链表问题.第3题_删除链表的中间节点和b分之a处的节点.删除中间节点;
+package 第2章_链表问题.第03题_删除链表的中间节点和b分之a处的节点.删除中间节点;
 
 import 第2章_链表问题.LinkedListTool;
 import 第2章_链表问题.ListNode;
