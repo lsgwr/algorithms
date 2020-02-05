cde7b8c7fc4919a34f0abd68b97de74771f82cba
19920625lsg <1648266192@qq.com>
2020-02-05 09:38:00 +0800

    最小路径和

--- Part7LeetCode/src/main/java/C12_动态规划/T64_最小路径和/Solution.java
+++ Part7LeetCode/src/main/java/C12_动态规划/T64_最小路径和/Solution.java
@@ -8,33 +8,33 @@
 package C12_动态规划.T64_最小路径和;
 
 class Solution {
-    private int[][] memo;
-
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        memo = new int[m][n];
+        int[][] memo = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                memo[i][j] = recursive(i, j, grid);
+                memo[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    memo[i][j] = grid[0][0];
+                    continue;
+                }
+                if (i == 0) {
+                    memo[i][j] = memo[i][j - 1] + grid[i][j];
+                    continue;
+                }
+                if (j == 0) {
+                    memo[i][j] = memo[i - 1][j] + grid[i][j];
+                    continue;
+                }
+                // i不为0，j也不为0，需要比较左上角和右下角的点了
+                memo[i][j] = Math.min(memo[i][j - 1], memo[i - 1][j]) + grid[i][j];
             }
         }
         return memo[m - 1][n - 1];
-    }
-
-    private int recursive(int i, int j, int[][] grid) {
-        if (memo[i][j] != 0) {
-            return memo[i][j];
-        }
-        if (i == 0 && j == 0) {
-            return grid[0][0];
-        }
-        if (i == 0) {
-            return recursive(i, j - 1, grid) + grid[i][j];
-        }
-        if (j == 0) {
-            return recursive(i - 1, j, grid) + grid[i][j];
-        }
-        return Math.min(recursive(i, j - 1, grid), recursive(i - 1, j, grid)) + grid[i][j];
     }
 }
