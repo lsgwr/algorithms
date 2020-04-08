c08ffcb9744921ad6c8c43b96e5faf96a4fa933d
lsgwr <1648266192@qq.com>
2020-04-08 20:34:25 +0800

    339. 嵌套列表权重和

---
+++ Part7LeetCode/src/main/java/C09_深度优先搜索/T339_嵌套列表权重和/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        return depthSum(nestedList, 1);
+    }
+
+    private int depthSum(List<NestedInteger> nestedList, int depth) {
+        int sum = 0;
+        for (NestedInteger integer : nestedList) {
+            if (integer.isInteger()){
+                sum+=integer.getInteger()*depth;
+            }else {
+                // 如果是数组，继续递归往下走
+                sum += depthSum(integer.getList(), depth+ 1);
+            }
+        }
+        return sum;
+    }
+}
