ddee67651c79fcf71d46fb26ef12bf2722ac67f8
19920625lsg <1648266192@qq.com>
2020-04-17 00:39:17 +0800

    490.迷宫

--- Part7LeetCode/src/main/java/C09_深度优先搜索/T490_迷宫/Solution.java
+++ Part7LeetCode/src/main/java/C09_深度优先搜索/T490_迷宫/Solution.java
@@ -46,7 +46,7 @@ class Solution {
         C = maze[0].length;
         this.visited = new boolean[R][C];
         this.grid = maze;
-        return dfs(start, destination, null);
+        return dfs(start, destination);
     }
 
     /**
@@ -54,83 +54,42 @@ class Solution {
      *
      * @param start       起点坐标
      * @param destination 目标点坐标
-     * @param preDir      上一次的移动方向
-     * @return
      */
-    private boolean dfs(int[] start, int[] destination, int[] preDir) {
+    private boolean dfs(int[] start, int[] destination) {
+        if (visited[start[0]][start[1]]) {
+            return false;
+        }
+        if (start[0] == destination[0] && start[1] == destination[1]) {
+            return true;
+        }
         // 标记当前访问节点
         visited[start[0]][start[1]] = true;
-        if (start[0] == destination[0] && start[1] == destination[1]) {
-            if (preDir != null) {
-                int nextR = start[0] + preDir[0];
-                int nextC = start[1] + preDir[1];
-                // 上一次的方向不为空，判断小球下一步的位置
-                if (!inGrid(nextR, nextC) || grid[nextR][nextC] == 1) {
-                    // 如果当前到达边界或者碰到墙了，下一步出不去了，此时达到我们的目的地而且停下来了
-                    return true;
-                } else {
-                    // 到达了目的地还能继续往后滑，那么目的地就标记为未访问，方便后面再次经过
-                    visited[start[0]][start[1]] = false;
-                }
-            } else {
-                // 说明一开始的起点就是终点，不需要动，直接返回true即可
-                return true;
-            }
-        } else {
-            // 没到达目的地，在允许的方向上继续滑行
-            if (preDir != null) {
-                int nextR = start[0] + preDir[0];
-                int nextC = start[1] + preDir[1];
-                // 上一次的方向上的下一个点不为空，判断小球下一步的位置
-                while (inGrid(nextR, nextC) && grid[nextR][nextC] == 0) {
-                    // 在栅格内，而且下一个点仍然是空地，则继续向下滑行
-                    start[0] = nextR;
-                    start[1] = nextC;
-                    // 标记访问节点
-                    visited[start[0]][start[1]] = true;
-                    // 不断更新下一个继续滑行的坐标
-                    nextR = start[0] + preDir[0];
-                    nextC = start[1] + preDir[1];
-                    if (start[0] == destination[0] && start[1] == destination[1]) {
-                        // 上一次的方向不为空，判断小球下一步的位置
-                        if (!inGrid(nextR, nextC) || grid[nextR][nextC] == 1) {
-                            // 如果当前到达边界或者碰到墙了，下一步出不去了，此时达到我们的目的地而且停下来了
-                            return true;
-                        }
-                    }
-
-                }
-            }
-        }
-        // 经过了上面的滑行，小球停下来了，继续朝可能的方向滑行
         for (int[] dir : dirs) {
+            // 计算下一个点
             int nextR = start[0] + dir[0];
             int nextC = start[1] + dir[1];
-            if (inGrid(nextR, nextC) && grid[nextR][nextC] == 0 && !visited[nextR][nextC]) {
-                // 改方向的下一个点在栅格内，而且是空地
-                if (dfs(new int[]{nextR, nextC}, destination, dir)) {
-                    return true;
-                }
+            // 该方向畅通地话就一直往下走
+            while (inGrid(nextR, nextC) && grid[nextR][nextC] == 0) {
+                nextR = nextR + dir[0];
+                nextC = nextC + dir[1];
+            }
+            // 一直走到无法畅通了，就dfs尝试换方向走。因为while最后多算了一次，所以dfs需要减去一次dir
+            if (dfs(new int[]{nextR - dir[0], nextC - dir[1]}, destination)) {
+                return true;
             }
         }
-        // 走到这里还没找到就是真没找到了
         return false;
     }
 
     /**
-     * [0,4]
-     * [4,4]
-     * <p>
-     * [4,3]
-     * [0,1]
-     * <p>
-     * [0,4]
-     * [1,2]
+     * [0,4] [4,4]  true
+     * [4,3] [0,1]  false
+     * [0,4] [1,2]  true
      */
     public static void main(String[] args) {
         int[][] maze = {{0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}, {1, 1, 0, 1, 1}, {0, 0, 0, 0, 0}};
-        int[] start = {4, 3};
-        int[] destination = {0, 1};
+        int[] start = {0, 4};
+        int[] destination = {1, 2};
         System.out.println(new Solution().hasPath(maze, start, destination));
     }
 }
