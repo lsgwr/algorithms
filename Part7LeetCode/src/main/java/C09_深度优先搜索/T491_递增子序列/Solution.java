32e13e54bd0ac9238271d157f973dc9cad92b2f1
19920625lsg <1648266192@qq.com>
2020-04-19 17:37:38 +0800

    T491.递增子序列  HashSet剪枝

--- Part7LeetCode/src/main/java/C09_深度优先搜索/T491_递增子序列/Solution.java
+++ Part7LeetCode/src/main/java/C09_深度优先搜索/T491_递增子序列/Solution.java
@@ -9,48 +9,42 @@
 package C09_深度优先搜索.T491_递增子序列;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Solution {
     public List<List<Integer>> findSubsequences(int[] nums) {
         List<List<Integer>> resultAll = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            List<Integer> result = new ArrayList<>();
-            dfs(i, i, nums, result, resultAll);
-        }
+        List<Integer> result = new ArrayList<>();
+        dfs(0, nums, result, resultAll);
         return resultAll;
     }
 
     /**
      * DFS遍历到的坐标
      *
-     * @param i         当前遍历到的点的下标
-     * @param last      当前节点的上一个节点
+     * @param cur       DFS遍历到的节点
      * @param nums      原始数组
      * @param result    当前的路径
      * @param resultAll 最终的结果
      */
-    private void dfs(int i, int last, int[] nums, List<Integer> result, List<List<Integer>> resultAll) {
-        // 能进来先把点加入进去
-        if (i == last) {
-            result.add(nums[i]);
-        } else {
-            if (nums[i] >= nums[last]) {
-                result.add(nums[i]);
-            }
-        }
+    private void dfs(int cur, int[] nums, List<Integer> result, List<List<Integer>> resultAll) {
         if (result.size() > 1) {
-            // 递增长度为2
-            if (!resultAll.contains(result)) {
-                // 把当前的递增序列加进去
-                resultAll.add(new ArrayList<>(result));
-            }
+            // 把当前的递增序列加进去
+            resultAll.add(new ArrayList<>(result));
         }
+        Set<Integer> set = new HashSet<>();
         // i后面所有的点都作为邻接点继续往下进行dfs
-        for (int j = i + 1; j < nums.length; j++) {
-            dfs(j, i, nums, result, resultAll);
-            if (result.size() >0){
-                // DFS退出要删除最后一个元素
+        for (int i = cur; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                // 在同一序列上相同的数字往后都是一样的，不用再重新加入了
+                continue;
+            }
+            if (result.size() == 0 || nums[i] >= result.get(result.size() - 1)) {
+                set.add(nums[i]);
+                result.add(nums[i]);
+                dfs(i + 1, nums, result, resultAll);
                 result.remove(result.size() - 1);
             }
         }
@@ -61,7 +55,7 @@ public class Solution {
      * {4, 3, 2, 1}
      */
     public static void main(String[] args) {
-        int[] nums = {4, 3, 2, 1};
+        int[] nums = {4, 6, 7, 7};
         System.out.println(new Solution().findSubsequences(nums));
     }
 }
