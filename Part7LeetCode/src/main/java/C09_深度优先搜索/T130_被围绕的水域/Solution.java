da69220c42c6eb81089a3559d9c185f62256d975
19920625lsg <1648266192@qq.com>
2020-04-22 23:20:21 +0800

    T130.被围绕的水域，用DFS做显然更简单

--- Part7LeetCode/src/main/java/C09_深度优先搜索/T130_被围绕的水域/Solution.java
+++ Part7LeetCode/src/main/java/C09_深度优先搜索/T130_被围绕的水域/Solution.java
@@ -1,77 +1,53 @@
-/***********************************************************
- * @Description : 被围绕的水域
- * https://leetcode-cn.com/problems/surrounded-regions/
- * @author      : 梁山广(Liang Shan Guang)
- * @date        : 2020/4/5 18:55
- * @email       : liangshanguang2@gmail.com
- ***********************************************************/
 package C09_深度优先搜索.T130_被围绕的水域;
 
-/**
- * 先对边界上每一个'O'做深度优先搜索，将与其相连的所有'O'改为'-'。然后遍历矩阵，将矩阵中所有'O'改为'X',将矩阵中所有'-'变为'O'
- */
-public class Solution {
+class Solution {
+    private int R;
+    private int C;
+    private char[][] board;
+    private boolean[][] visited;
+    private final int[][] dirs = {{0, 1},{1, 0}, {0, -1}, {-1, 0}};
 
-    /**
-     * 当前点求上右下左四个点时用到的矢量差,dirs 是directions的意思
-     */
-    private int[][] dirs = {
-            {-1, 0},
-            {0, 1},
-            {1, 0},
-            {0, -1}
-    };
+    private boolean inGrid(int r, int c){
+        return r >=0 && r < R && c >= 0 && c < C;
+    }
 
-    private int R, C;
-
-    /**
-     * 判断第x行第y列的点(x, y)是否在grid所在的范围内
-     */
-    private boolean inArea(int x, int y) {
-        return x >= 0 && x < R && y >= 0 && y < C;
+    private boolean onBorder(int r, int c){
+        return r == 0 || r == R -1 || c == 0 || c == C - 1;
     }
 
     public void solve(char[][] board) {
-        if (board == null || board.length == 0) {
+        if(board == null || board.length == 0){
             return;
         }
         R = board.length;
         C = board[0].length;
-        for (int i = 0; i < R; i++) {
-            dfs(board, i, 0);
-            dfs(board, i, C - 1);
-        }
-        for (int j = 0; j < C; j++) {
-            dfs(board, 0, j);
-            dfs(board, R - 1, j);
-        }
-        // 标记完毕，开始刷新O和X
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (board[i][j] == 'O') {
-                    board[i][j] = 'X';
+        this.board = board;
+        visited = new boolean[R][C];
+        // 从所有的边界点O向里面进行DFS遍历，能联通的所有O标记为visited，最终没被访问到地O就是该被X填充地
+        for(int r = 0; r < R; r++){
+            for(int c = 0; c < C; c++){
+                if(!visited[r][c] && onBorder(r, c) && board[r][c] == 'O'){
+                    dfs(r, c);
                 }
-                if (board[i][j] == '-') {
-                    board[i][j] = 'O';
+            }
+        }
+        // 修改board
+        for(int r = 0; r < R; r++){
+            for(int c = 0; c < C; c++){
+                if(!visited[r][c] && board[r][c] == 'O'){
+                    board[r][c] = 'X';
                 }
             }
         }
     }
 
-    public void dfs(char[][] board, int x, int y) {
-        if (board[x][y] != 'O') {
-            // 必须从值为O的点开始DFS
-            return;
-        }
-        // 把没有被X包围的O先改成-，方便最后二次循环的时候改成O，被包围的O不会被改成-
-        board[x][y] = '-';
-        // 遍历节点(x, y)所有的邻接点,把和其连接的所有O点都标记成-，方便后面二次修改为X
-        for (int[] dir : dirs) {
-            int nextX = x + dir[0];
-            int nextY = y + dir[1];
-            // 点(nextX, nextY)必须在board区域内 + 必须是O，这样就把所有和边界O连在一起的O全变成
-            if (inArea(nextX, nextY) && board[nextX][nextY] == 'O') {
-                dfs(board, nextX, nextY);
+    private void dfs(int r, int c){
+        visited[r][c] = true;
+        for(int[] dir : dirs){
+            int rNext = r + dir[0];
+            int cNext = c + dir[1];
+            if(inGrid(rNext, cNext) && !visited[rNext][cNext] && board[rNext][cNext] == 'O'){
+                dfs(rNext, cNext);
             }
         }
     }
