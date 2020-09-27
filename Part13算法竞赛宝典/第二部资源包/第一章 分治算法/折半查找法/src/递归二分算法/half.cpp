48b117358d772e85755788e633f76b09c712775c
19920625lsg <1648266192@qq.com>
2020-09-27 23:26:46 +0800

    折半查找法 递归实现

--- Part13算法竞赛宝典/第二部资源包/第一章 分治算法/折半查找法/src/递归二分算法/half.cpp
+++ Part13算法竞赛宝典/第二部资源包/第一章 分治算法/折半查找法/src/递归二分算法/half.cpp
@@ -1,43 +1,37 @@
-//递归的折半查找法
+// 递归法
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#define Max 10001
-using namespace std;
-int a[Max],key; 
 
-int search(int bot,int top)
-{
-  int mid;
-  if(top>=bot)
-  {
-    mid=(top+bot)/2;//取中间值mid 
-    if(key==a[mid]) //如果相等,则打印该数 
-    {
-      cout<<mid<<endl;
-      return 0;
-    }   
-    else if(key<a[mid])  //如x小于中间值, 则取前半段 
-      search(bot,mid-1);
-    else               //如x大于中间值,则取后半段 
-      search(mid+1,top);
-  }      
-  else 
-  {
-      printf("-1\n");
-      return 0;
-  }    
+#define MAX 10001
+
+using namespace std;
+
+int a[MAX], target;
+
+int search(int left, int right) {
+    int mid;
+    if (left <= right) {
+        mid = (left + right) / 2;
+        if (target == a[mid]) {
+            cout << mid << endl;
+            return 0;
+        } else if (target < a[mid]) {
+            search(left, right - 1);
+        } else {
+            search(mid + 1, right);
+        }
+    } else {
+        cout << -1 << endl;
+        return 0;
+    }
 }
 
-int main()
-{
-   freopen("half.in","r",stdin);
-   freopen("half.out","w",stdout); 
-   int n; 
-   cin>>n;
-   for(int i=1;i<=n;++i)
-     cin>>a[i]; 
-   cin>>key;
-   search(1,n);
-   return 0;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    cin >> target;
+    search(1, n);
+    return 0;
 }
